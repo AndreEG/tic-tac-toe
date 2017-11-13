7e3fae800f267e24e6fad470bb251a90a78bb42b
AndreEG <andrea_206_93@hotmail.com>
2017-11-13 13:43:34 -0500

    añadiendo js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
 window.onload = function() {
-    var board=document.querySelector('.michi-js');
-    board.addEventListener('click',addX);
-    };
-    var centinel=true;
-    function addX(event){
-      if(event.target.matches('td') && event.target.textContent === ''){
-          event.target.textContent='X';
-          centinel=!centinel;
-        } else {
-          event.target.textContent='O';
-          centinel=!centinel;
-        }
-      }
+  var board = document.querySelector('.board-js');
+  board.addEventListener('click',addX);
+};
+var centinel = true;
+function addX(event) {
+  if(event.target.matches('td') && event.target.textContent === ''){
+    if (centinel)
+      event.target.textContent = 'X';
+    else
+      event.target.textContent = 'O';
+    centinel = !centinel;
+  }
+}
+
+td1 = document.getElementById('one');
+var td2 = document.getElementById('two');
+var td3 = document.getElementById('three');
+var td4 = document.getElementById('four');
+var td5 = document.getElementById('five');
+var td6 = document.getElementById('six');
+var td7 = document.getElementById('seven');
+var td8 = document.getElementById('eight');
+var td9 = document.getElementById('nine');
+    
+function win() {
+  if(td1.textContent==='X' && td2.textContent==='X' && td3.textContent==='X'){
+      alert('Gano primer jugador');
     }
+}
+win();
